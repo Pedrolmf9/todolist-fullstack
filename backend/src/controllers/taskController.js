da817eb01642307e7c9215fd85f9ea433028e263
Pedrolmf9 <pedroluizmf@gmail.com>
2023-11-08 15:01:06 -0300

    Create middleware to validate post

--- backend/src/controllers/taskController.js
+++ backend/src/controllers/taskController.js
@@ -1,7 +1,18 @@
-const getAll = (request, response) => {
-    return response.status(200).json({message: 'tudo certo com controller!'});
+const taskModel = require('../models/tasksModel')
+
+const getAll = async (request, response) => {
+    const tasks = await taskModel.getAll();
+
+    return response.status(200).json(tasks);
+};
+
+const createTask = async (request, response) => {
+    const createdTask = await taskModel.createTask(request.body);
+
+    return response.status(201).json(createdTask);
 };
 
 module.exports = {
-    getAll
+    getAll,
+    createTask,
 };
--- backend/src/router.js
+++ backend/src/router.js
@@ -2,8 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 
-const tasksController = require('./controllers/taskController')
+const tasksController = require('./controllers/taskController');
+const taskMiddleware = require('./middlewares/taskMiddleware');
 
 router.get('/tasks', tasksController.getAll);
+router.post('/tasks', taskMiddleware.validateBody, tasksController.createTask);
 
 module.exports = router;
